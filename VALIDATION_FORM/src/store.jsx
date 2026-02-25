49d15a477ba1cf27248e41c1a5b0648f66238ba2
HARSHRAJSINH PARMAR <softwareallian@gmail.com>
2026-02-25 10:10:33 +0530

    DELTED VALIDATION_FORM

--- VALIDATION_FORM/src/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import formReducer from './formSlice'; // Ensure the path is correct
-
-export const store = configureStore({
-    reducer: {
-        registration: formReducer,
-    },
-});
